79309231096fe86f42fa558d8f9703003ae62988
Jesús Suárez Molina <jesuss@apolo.local>
2018-11-29 13:23:53 -0300

    Structure refactor.

--- src/app/master-page/master-page.module.ts
+++ src/app/master-page/master-page.module.ts
@@ -3,13 +3,11 @@ import { CommonModule } from '@angular/common';
 import { MasterPageComponent } from './master-page.component';
 import { MaterialModule } from '../material/material.module';
 import { MasterPageRoutingModule } from './master-page-routing.module';
-import { MessagingModule } from '../messaging/messaging.module';
 import { LoginModule } from '../auth/login/login.module';
 
 @NgModule({
   declarations: [MasterPageComponent],
   imports: [
-    MessagingModule,
     CommonModule,
     MasterPageRoutingModule,
     MaterialModule,
--- src/app/tracking-map/tracking-map.module.ts
+++ src/app/tracking-map/tracking-map.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { TrackingMapRoutingModule } from './tracking-map-routing.module';
 import { TrackingMapComponent } from './tracking-map.component';
 import { TrackingMapService } from './tracking-map.service';
+import { MaterialModule } from '../material/material.module';
+import { MessagingModule } from './messaging/messaging.module';
 
 @NgModule({
   declarations: [TrackingMapComponent],
   imports: [
+    MessagingModule,
     CommonModule,
-    TrackingMapRoutingModule
+    TrackingMapRoutingModule,
+    MaterialModule
   ],
   providers: [
     TrackingMapService
