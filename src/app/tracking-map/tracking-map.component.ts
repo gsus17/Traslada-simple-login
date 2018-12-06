6b6abde51d7aed2e0bf965e29be0cd59a7456094
Jesús Suárez Molina <jesuss@apolo.local>
2018-12-06 13:00:35 -0300

    (ST) Mousetrap library included.

--- src/app/tracking-map/tracking-map.component.ts
+++ src/app/tracking-map/tracking-map.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import * as Mousetrap from 'Mousetrap';
+import { MatSidenavContainer } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-tracking-map',
@@ -6,11 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tracking-map.component.sass']
 })
 export class TrackingMapComponent implements OnInit {
-  constructor() { }
+  @ViewChild(MatSidenavContainer) sidenavContainer: MatSidenavContainer;
+
+  /**
+   * Toggle assistant sidebar flag.
+   */
+  public openAssistantSidebar: boolean;
+
+  /**
+   * Toggle message sidebar flag.
+   */
+  public openMessageSidebar: boolean;
+  constructor() {
+  }
 
   ngOnInit() {
     const windows: any = <any>window;
     const map = new windows.google.maps.Map(document.getElementById('map'), { zoom: 4, center: { lat: -25.344, lng: 131.036 } });
+    this.listenMoustrapEvents();
   }
 
+  /**
+   * Init listener of mousetrap.
+   */
+  private listenMoustrapEvents() {
+
+    Mousetrap.bind('a', () => {
+      console.log('Funfiona');
+      this.openAssistantSidebar = !this.openAssistantSidebar;
+    }, 'keyup');
+
+    Mousetrap.bind('m', () => {
+      console.log('Funfiona');
+      this.openMessageSidebar = !this.openMessageSidebar;
+    }, 'keyup');
+  }
 }
